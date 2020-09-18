2f64a17fec90afd235372654c6ad2431478551f5
csslick <digitone@naver.com>
2020-09-18 23:27:03 +0900

    111

---
+++ js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var f = function f() {
+  var es6 = 'ECMA Script 2015';
+  console.log(es6);
+};
+
+f();
---
+++ src/main.js
@@ -0,0 +1,7 @@
+
+const f = () => {
+  let es6 = 'ECMA Script 2015';
+  console.log(es6);
+}
+
+f();
